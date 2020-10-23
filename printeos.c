42d0792fc90f85d15154783177786d198b30b614
UberSquid <63605999+UberSquid@users.noreply.github.com>
2020-10-23 18:05:01 -0300

    pongo un poco mas lindos los prints

--- printeos.c
+++ printeos.c
@@ -4,9 +4,9 @@ int Determinador_roor (int, int, int, int) __attribute__((cdecl));
 
 int main(){
     int a, b, c, d;
-    printf("ingrese 4 numeros para generar la matriz");
+    printf("ingrese 4 numeros para generar la matriz:\n con el formato %%i %%i %%i %%i \n  ");
     scanf("%i %i %i %i",&a,&b,&c,&d);
     a = Determinador_roor(a, b, c, d);
-    printf("el determinante de la matriz es: %i", a);
+    printf("el determinante de la matriz es: %i \n", a);
     return 0;
 }
